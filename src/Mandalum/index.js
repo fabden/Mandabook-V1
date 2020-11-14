55630f922e48942cd14e526540eb709a34938383
FabDen <fabden@gmail.com>
2020-11-14 16:32:50 +0100

    first comit

---
+++ src/Mandalum/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function Mandalum() {
+    return (
+      <div>
+        <svg width="300" height="200">
+            <polygon points="100,10 40,198 190,78 10,78 160,198"className="svgg"/>
+        </svg>
+      </div>
+    );
+  }
+  export default Mandalum;
