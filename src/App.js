f92d66097989565833618df591882096adf1677d
FabDen <fabden@gmail.com>
2020-11-26 13:59:49 +0100

    creation descrition

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Mandalum from '../src/Mandalum';
 import Header from '../src/Header';
 import Footer from '../src/Footer';
+import  Description from '../src/Description';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
   <div className="app"> 
     <Header />
+    <Description />
     <section className="section_application">      
         <Mandalum mand={2} type={"prenom"}/>
         <Mandalum mand={1} type={"citation"}/>
---
+++ src/Description/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+
+
+function Description (){
+
+    return(
+    <section className='section_description'> 
+        <h2 className='section_description_title'>Bienvenu sur notre générateur de marque pages</h2>
+        <p className='section_description_text'>chaque même messages et même prénoms généreras toujours le même motif <br/> les espaces, majuscules et caractère spéciaux sont pris en compte. amusez-vous bien....        
+        </p>                              
+    </section>
+    )   
+
+};
+
+export default Description;
